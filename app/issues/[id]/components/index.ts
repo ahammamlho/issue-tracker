2242d57542ca98d215f75e3d397836fa58d532ca
Lhoussaine Ahammam <lahammam@e2r2p13.1337.ma>
2023-10-15 13:46:47 +0100

    add delete button

--- app/issues/[id]/components/index.ts
+++ app/issues/[id]/components/index.ts
@@ -1,2 +1,3 @@
 export { default as EditIssueButton } from "./EditIssueButton";
+export { default as DeleteIssueButton } from "./DeleteIssueButton";
 export { default as IssueDetails } from "./IssueDetails";
